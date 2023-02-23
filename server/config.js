908812f2a955b02936f3af36659844297d54a119
Developer889 <124581162+Developer889@users.noreply.github.com>
2023-02-23 20:46:19 +0500

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,10 @@
+// export default{
+//     ATLAS_URI:"mongodb://localhost:27017",
+//     JWT_SECRET:"secret"
+// }
+export default {
+    JWT_SECRET : "secret",
+    EMAIL: "jaclyn.grady17@ethereal.email", // testing email & password
+    PASSWORD : "QKaFqAhdeTqJjj4GQB",
+    ATLAS_URI: "mongodb+srv://admin2:admin2@cluster0.tjlcrf0.mongodb.net/?retryWrites=true&w=majority"
+}
